b5fe8cfebb59c23f070523fcb581d901c4fcc8bc
Strong-Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-06-10 12:54:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ if __name__ == "__main__":
     urls = [
         "https://www.50-a.org/data/nypd/officers.csv",
         "https://www.50-a.org/data/nypd/ranks.csv"
+        "https://www.50-a.org/data/nypd/discipline.csv"
+        "https://www.50-a.org/data/nypd/documents.csv"
+        "https://www.50-a.org/data/nypd/awards.csv"
     ]
     
     # Folder to save the downloaded files
